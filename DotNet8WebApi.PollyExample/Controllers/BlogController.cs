60375ea52fcf27058d101898f02e00c45e81578c
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-31 15:49:28 +0630

    Add project files.

---
+++ DotNet8WebApi.PollyExample/Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+﻿using DotNet8WebApi.PollyExample.Db;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Polly;
+using Polly.Retry;
+
+namespace DotNet8WebApi.PollyExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBlogs()
+        {
+            var policy = Policy.Handle<Exception>()
+                .WaitAndRetryAsync(3, attempt => TimeSpan.FromMicroseconds(1000 * attempt));
+
+            var lst = await policy.ExecuteAsync(() => _context.Tbl_Blogs.AsNoTracking().OrderByDescending(x => x.BlogId).ToListAsync());
+            return Ok(lst);
+        }
+    }
+}
---
+++ DotNet8WebApi.PollyExample/Db/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8WebApi.PollyExample.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Tbl_Blog> Tbl_Blogs { get; set; }
+    }
+}
